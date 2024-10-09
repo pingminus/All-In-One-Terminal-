bc8a53ab16c7cc4476f452eb3a002299c8cb8023
pingminus <niklasdioxid@gmail.com>
2024-10-09 20:05:20 +0200

    update

--- command_handler.py
+++ command_handler.py
@@ -43,9 +43,9 @@ class CommandHandler:
     def display_help(self):
         """Display help commands."""
         print()
-        print("     -e       Exit               -g       Games")
-        print("     -u       LinkFetcher        -k       Start Key Logger")
-        print("     -stopkeylogger Stop Key Logger  -hidden")  # Help for stopping key logger
+        print("     -e       Exit                   -g       Games")
+        print("     -u       LinkFetcher            -k       Start Key Logger")
+        print("     -stopkeylogger Stop Key Logger  -k       hidden Key Logger")  # Help for stopping key logger
         print("     -color   Change Color")
         print("       -list  Color List")
 
